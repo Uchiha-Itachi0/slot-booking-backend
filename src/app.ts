5f3dd7ab47ec9764448386ca4f0b58201c85fe38
Anubhav Shukla <anubhav008shukla@gmail.com>
2025-01-05 18:32:42 +0530

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express"
 import errorHandler from "./middlewares/errorHandler";
 import slotRoutes from "./routes/slotRoutes";
 import bookingRoutes from "./routes/bookingRoutes";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 
 require("dotenv").config();
@@ -9,6 +11,8 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(cors())
+
 app.use('/api/slots', slotRoutes);
 app.use('/api/booking', bookingRoutes);
 
